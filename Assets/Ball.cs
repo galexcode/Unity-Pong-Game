7092ccc5fa0d28f5ad8dfa0478b329d46a9e57b3
Brendon O'Laney <brendon.olaney@me.com>
2013-07-29 19:52:40 -0700

    tightened up physics

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -6,10 +6,21 @@ public class Ball : MonoBehaviour {
     // Use this for initialization
     void Start () {
         float initialVelocity = 700.0f;
-        float xVel = Random.value;
-        float zVel = Random.value;
+        float xVel = Random.Range(1, 10);
+        float zVel = Random.Range(1, 2);
 
+        // Randomly adjust direction so the ball doesn't always fly up/right.
+        if (Random.value > 0.5) {
+            xVel *= -1;
+        }
+
+        if (Random.value > 0.5) {
+            zVel *= -1;
+        }
+
+        // Reset position and velocity.
         transform.position = new Vector3(0, 0, 0);
+        rigidbody.velocity = new Vector3(0, 0, 0);
 
         Vector3 baseVector = new Vector3(xVel, 0, zVel);
         Vector3 startVector = initialVelocity * baseVector.normalized;
@@ -25,6 +36,7 @@ public class Ball : MonoBehaviour {
     }
 
     void FixedUpdate () {
+
     }
     
     void OnCollisionEnter(Collision collision) {
@@ -40,6 +52,24 @@ public class Ball : MonoBehaviour {
             GameGUI gui = camera.GetComponent<GameGUI>();
             gui.IncRightScore();
             this.Start(); 
+        }
+
+        else if (collision.collider.name == "PaddleLeft") {
+            GameObject paddle = GameObject.Find("PaddleLeft");
+            float pPos = paddle.transform.position.z;
+            float bPos = transform.position.z;
+            rigidbody.AddForce(
+                new Vector3(0, 0, (8.0f * (bPos - pPos))),
+                ForceMode.Impulse);
+        }
+
+        else if (collision.collider.name == "PaddleRight") {
+            GameObject paddle = GameObject.Find("PaddleRight");
+            float pPos = paddle.transform.position.z;
+            float bPos = transform.position.z;
+            rigidbody.AddForce(
+                new Vector3(0, 0, (8.0f * (bPos - pPos))),
+                ForceMode.Impulse);
         }
     }
 }
